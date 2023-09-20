bd74ffffd4f0d720019112a051e825559d6421c4
Dean Perillo <perilldj@sunypoly.edu>
2023-09-20 00:33:27 -0400

    Added matrix documentation

--- CPP/source/Matrix.cpp
+++ CPP/source/Matrix.cpp
@@ -1,5 +1,11 @@
 #include "../header/Matrix.h"
 
+/*
+    Matrix::Matrix(int col_, int row_)
+    Description: Constructor of the matrix class, defines the size of the matrix
+                 and initializes the internal two-dimensional vector.
+*/
+
 Matrix::Matrix(int col_, int row_) :
     col(col_), row(row_) {
     arr.resize(col);
@@ -7,12 +13,22 @@ Matrix::Matrix(int col_, int row_) :
         arr[i].resize(row);
 }
 
+/*
+    float Matrix::Get(int col, int row)
+    Description: Returns the value at the specified column and row locations.
+*/
+
 float Matrix::Get(int col, int row) {
     if(col >= 0 && col < this->col && row >= 0 && row < this->row )
         return arr[col][row];
     std::cerr << "Matrix.cpp - [ERROR] - Index out of bounds." << std::endl;
     return 0.0f;
 }
+
+/*
+    void Matrix::Set(int col, int row, float value)
+    Description: Sets the value at the specified column and row locations to the new provided value.
+*/
 
 void Matrix::Set(int col, int row, float value) {
     if(col >= 0 && col < this->col && row >= 0 && row < this->row ) {
@@ -22,11 +38,22 @@ void Matrix::Set(int col, int row, float value) {
     std::cerr << "Matrix.cpp - [ERROR] - Index out of bounds." << std::endl; 
 }
 
+/*
+    void Matrix::Scale(float s, Matrix &mat)
+    Description: Scales the provided matrix by the a scalar.
+*/
+
 void Matrix::Scale(float s, Matrix &mat) {
     for(int i = 0; i < mat.col; i++)
         for(int j = 0; j < mat.row; j++)
             mat.arr[i][j] *= s;
 }
+
+/*
+    void Matrix::Scale(float s, Matrix &mat, Matrix &out)
+    Description: Scales the provided matrix by a scalar, but stores the result in
+                 the out matrix passed by reference.
+*/
 
 void Matrix::Scale(float s, Matrix &mat, Matrix &out) {
     if(!IsEqualSize(mat, out))
@@ -36,6 +63,12 @@ void Matrix::Scale(float s, Matrix &mat, Matrix &out) {
             out.Set(i, j, mat.Get(i, j) * s);
 }
 
+/*
+    void Matrix::Add(Matrix &mat1, Matrix &mat2, Matrix &out)
+    Description: Adds two same size matrices together, stores the result in
+                 the out matrix passed by reference.
+*/
+
 void Matrix::Add(Matrix &mat1, Matrix &mat2, Matrix &out) {
     if(!IsEqualSize(mat1, mat2) || !IsEqualSize(mat2, out))
         return;
@@ -43,6 +76,11 @@ void Matrix::Add(Matrix &mat1, Matrix &mat2, Matrix &out) {
         for(int j = 0; j < mat1.row; j++)
             out.Set(i, j, mat1.Get(i, j) + mat2.Get(i, j));
 }
+
+/*
+    bool Matrix::IsEqualSize(MAtrix &mat1, Matrix &mat2)
+    Description: Checks if the two provided matrixes are the same size.
+*/
 
 bool Matrix::IsEqualSize(Matrix &mat1, Matrix &mat2) {
     return (mat1.col == mat2.col && mat1.row == mat2.row);
